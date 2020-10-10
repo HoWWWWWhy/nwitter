54ba1e5feecbba5327c0ad231e699a2886fb977f
BYUNSEHEE <howwwwwhy@gmail.com>
2020-10-11 01:24:15 +0900

    configure routers

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Router } from "react-router-dom/cjs/react-router-dom.min";
+import AppRouter from "./Router";
+
+const App = () => {
+  return <AppRouter />;
+};
+
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP4ml9_za4rFH97wPHZke6EI2rYy7G6vQ",
-  authDomain: "nwitter-99c72.firebaseapp.com",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: "https://nwitter-99c72.firebaseio.com",
   projectId: "nwitter-99c72",
   storageBucket: "nwitter-99c72.appspot.com",
   messagingSenderId: "854569373034",
-  appId: "1:854569373034:web:c782cf9601854f80f02913"
+  appId: "1:854569373034:web:c782cf9601854f80f02913",
 };
 
 // Initialize Firebase
